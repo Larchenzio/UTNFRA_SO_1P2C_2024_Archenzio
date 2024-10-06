117c2c63a8533f74d422606b7751e093657c2703
Larchenzio <leandro.archenzio@hotmail.com>
2024-10-06 06:46:21 +0000

    Actualizacion del script Punto_D.sh: Estructura asimetrica

---
+++ RTA_SCRIPT_Examen_20241006/Punto_D.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Creo estructura de directorio"
+
+mkdir -p $HOME/Estructura_Asimetrica/{{correo,cliente}/cartas_{1..100},correo/carteros_{1..10}}
+
+echo "Mostramos estructura"
+
+tree $HOME/Estructura_Asimetrica/ --noreport | pr -T -s' ' -w 80 --column 4
