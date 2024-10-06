e36bcd512e853744078b9c417fdbe75dd30f6c78
Larchenzio <leandro.archenzio@hotmail.com>
2024-10-06 03:12:38 +0000

    Eliminación de scripts antiguos y modificación de Punto_A.sh en RTA_SCRIPT_Examen_20241006

--- RTA_SCRIPT_Examen_20241003/Punto_A.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Creo estructura de directorio"
-
-mkdir -p $HOME/Examenes_UTN/{alumno_{1..3}/parcial_{1..3},profesores}
-
-tree /home/vagrant/Examenes_UTN
--- RTA_SCRIPT_Examen_20241006/Punto_A.sh
+++ RTA_SCRIPT_Examen_20241006/Punto_A.sh
@@ -4,5 +4,5 @@ echo "Creo estructura de directorio"
 
 sudo mkdir -p /Examenes_UTN/{alumno_{1..3}/parcial_{1..3},profesores}
 
-echo "Mostramos estrcutura"
-tree /Examenes_UTN
+
+
